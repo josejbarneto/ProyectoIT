a5ef43ade4c4c766181e62757de52b574fadd522
nenitomg <nenitomg@gmail.com>
2020-05-31 22:44:25 +0200

    minor

---
+++ youevent/src/java/acciones/sesiones/getListaAnuncios.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acciones.sesiones;
+
+import acciones.anuncios.*;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.List;
+import modelo.dao.anuncioDAO;
+import modelo.dao.eventoDAO;
+import modelo.entidades.Anuncio;
+
+/**
+ *
+ * @author Carlos
+ */
+public class getListaAnuncios extends ActionSupport {
+    
+    private List<Anuncio> listaAnuncios;
+
+    public List<Anuncio> getListaAnuncios() {
+        return listaAnuncios;
+    }
+
+    public void setListaAnuncios(List<Anuncio> listaAnuncios) {
+        this.listaAnuncios = listaAnuncios;
+    }
+
+
+    
+    public getListaAnuncios() {
+    }
+    
+    @Override
+    public String execute() throws Exception {
+        listaAnuncios = new anuncioDAO().getAll();
+        return SUCCESS;
+    }
+    
+}
